16f87c396c6c73e969c1557db3c49351e8b3c8a0
ASKEROVA SABINA <sabina.askerova@etu.unistra.fr>
2024-11-03 00:03:27 +0100

    static gm, long to train. (0.193, 0.093, 80)

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 import torch
-import numpy as np
+from torch import nn
 import os
 from torch.distributions.multivariate_normal import MultivariateNormal
-
+import numpy as np
 class StaticGMM:
-    def __init__(self, n_components=10, latent_dim=100, c=1.0, sigma=0.1):
+    def __init__(self, n_components=10, latent_dim=100, c=0.1, sigma=0.15):
         self.n_components = n_components
         self.latent_dim = latent_dim
         self.c = c
@@ -40,22 +40,25 @@ class StaticGMM:
     
 static_gmm = StaticGMM(n_components=10, latent_dim=100, c=0.1, sigma=0.15)
 
-def D_train(x, G, D, D_optimizer, criterion):
-    #=======================Train the discriminator=======================#
-    D.zero_grad()
 
+def D_train(x, G, D, D_optimizer, criterion):
+    D.zero_grad()
     # train discriminator on real
-    x_real, y_real = x, torch.ones(x.shape[0], 1)
-    x_real, y_real = x_real.cuda(), y_real.cuda()
-    D_output = D(x_real)
-    D_real_loss = criterion(D_output, y_real)
+    x_real = x.cuda() ###
+    y_real = torch.rand(x.shape[0], 1) * 0.2 + 0.8 ###
+    y_real = y_real.cuda() ###
+
+    D_output = D(x_real) ###
+    D_real_loss = criterion(D_output, y_real) ###
     D_real_score = D_output
 
     # train discriminator on fake
-    # Use Static GMM instead of standard normal distribution
-    z = static_gmm.sample(batch_size=x.shape[0])
-    x_fake, y_fake = G(z), torch.zeros(x.shape[0], 1).cuda()
-    D_output = D(x_fake)
+    # z = torch.randn(x.shape[0], 100).cuda()  ###
+    z = static_gmm.sample(batch_size=x.shape[0]).cuda() 
+    x_fake = G(z) ###
+    y_fake = torch.rand(x.shape[0], 1).cuda() * 0.2 ###
+    D_output = D(x_fake) ###
+
     D_fake_loss = criterion(D_output, y_fake)
     D_fake_score = D_output
 
@@ -68,29 +71,37 @@ def D_train(x, G, D, D_optimizer, criterion):
 
 
 def G_train(x, G, D, G_optimizer, criterion):
-    #=======================Train the generator=======================#
-    G.zero_grad()
+    G.zero_grad() ###
+    # z = torch.randn(x.shape[0], 100).cuda() ###
+    z = static_gmm.sample(batch_size=x.shape[0]).cuda()
+    y = torch.ones(x.shape[0], 1).cuda() ###
 
-    # Use Static GMM instead of standard normal distribution
-    z = static_gmm.sample(batch_size=x.shape[0])
-    y = torch.ones(x.shape[0], 1).cuda()
+    G_output = G(z).cuda() ###
+    D_output = D(G_output).cuda() ###
+    x= x.cuda() ##
 
-    G_output = G(z)
-    D_output = D(G_output)
-    G_loss = criterion(D_output, y)
+    # adversarial loss
+    # G_adv_loss = criterion(D_output, y)
 
+    # reconstruction loss (L2)
+    # G_recon_loss = ((x - G_output) ** 2).mean()
+
+    # G_loss = G_adv_loss + 0.1 * G_recon_loss  # adjust the weight of recon  loss as needed
+    G_loss = criterion(D_output, y) ###
     # gradient backprop & optimize ONLY G's parameters
-    G_loss.backward()
-    G_optimizer.step()
+    G_loss.backward() ###
+    G_optimizer.step() ###
 
     return G_loss.data.item()
 
 
 def save_models(G, D, folder):
+    os.makedirs(folder, exist_ok=True)
     torch.save(G.state_dict(), os.path.join(folder,'G.pth'))
     torch.save(D.state_dict(), os.path.join(folder,'D.pth'))
 
+
 def load_model(G, folder):
     ckpt = torch.load(os.path.join(folder,'G.pth'))
-    G.load_state_dict({k.replace('module.', ''): v for k, v in ckpt.items()})
+    G.load_state_dict({k.replace('module.', ''): v for k, v in  ckpt.items()})
     return G
