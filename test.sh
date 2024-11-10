a759da7c8d90fa467b6c5c1e1313a1b40f6872ea
ASKEROVA SABINA <sabina.askerova@etu.unistra.fr>
2024-11-10 21:52:10 +0100

    test.sh

--- test.sh
+++ test.sh
@@ -3,24 +3,26 @@ if [ ! -f results_lsgan.txt ]; then
     touch results_lsgan.txt
 fi
 if [ ! -f checkpoints/G_0.05.pth ]; then
-    touch G_0.05.pth
+    touch checkpoints/G_0.05.pth
 fi
 if [ ! -f checkpoints/D_0.05.pth ]; then
-    touch D_0.05.pth
+    touch checkpoints/D_0.05.pth
 fi
+
 python3 train.py --noise_factor 0.05 --gname "G_0.05.pth" --dname "D_0.05.pth"
 python3 generate.py --gname "G_0.05.pth"
 echo "--noise_factor 0.05:" >> results_lsgan.txt
 python -m pytorch_fid samples test_images >> results_lsgan.txt
 python improved_precision_recall.py test_images samples --num_samples 10000 >> results_lsgan.txt
+
 if [ ! -f checkpoints/G_0.01.pth ]; then
-    touch G_0.01.pth
+    touch checkpoints/G_0.01.pth
 fi
 if [ ! -f checkpoints/D_0.01.pth ]; then
-    touch D_0.01.pth
+    touch checkpoints/D_0.01.pth
 fi
+
 python3 train.py --noise_factor 0.01 --gname "G_0.01.pth" --dname "D_0.01.pth"
-echo "python3 train.py --noise_factor 0.01 --gname "G_0.01.pth" --dname "D_0.01.pth""
 python3 generate.py --gname "G_0.01.pth"
 
 echo "--noise_factor 0.01:" >> results_lsgan.txt
